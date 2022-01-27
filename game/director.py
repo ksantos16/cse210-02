02fc4a12a4a68893c491043aec316e46db04b0c6
ksantos16 <san21087@byui.edu>
2022-01-26 20:11:59 -0500

    add comments to director

--- game/director.py
+++ game/director.py
@@ -2,22 +2,21 @@ from game.card import Card
 
 
 class Director:
-    """A person who directs the game. 
-    
-    The responsibility of a Director is to control the sequence of play.
+    """Director is going to direct the game and control the sequence of the play.
 
-    Attributes:
-        cards (List[card]): A list of Card instances.
-        is_playing (boolean): Whether or not the game is being played.
-        total_score (int): The score for the entire game.
-    """
+    Attibutes:
+    List = self.card = [] : this will create a list from the card_generator and hold the values
+    Boolean = self.is_playing = True : This is saying the game will continue till the user says (n or NO)
+    Int = self.score = 300 : This is the amount you start off with, based on user input the anount will either increase or decrease
+    Boolean = self.playerchoice = None : This will hold false till the user inputs a choice."""
 
     def __init__(self):
         """Constructs a new Director.
-        
+
         Args:
             self (Director): an instance of Director.
         """
+        # sets default values
         self.cards = []
         self.is_playing = True
         self.total_score = 300
@@ -26,11 +25,13 @@ class Director:
         self.cards.append(card)
 
     def start_game(self):
-        """Starts the game by running the main game loop.
-        
-        Args:
-            self (Director): an instance of Director.
-        """
+        """Method/Function:
+        Starts running the game and will continue till the user answer no (n) to continue.
+
+         Args:
+        self (game_master): An instance of game_master"""
+        # central control function/switchboard
+
         while self.is_playing:
 
             self.get_inputs()
@@ -40,24 +41,29 @@ class Director:
         print("Thanks for playing!")
 
     def get_inputs(self):
-        """Ask the user if they want to keep playing.
+        """Method/Function:
+        Asks the user if the card that is not displayed yet is y or n
 
         Args:
-            self (Director): An instance of Director.
-        """
+        self (game_master): An instance of game_master"""
+        # gets user inputs
         draw_card = input("Play? [y/n] ")
         self.is_playing = (draw_card == "y")
         print(self.is_playing)
-       
-    def do_updates(self):
-        """Updates the player's score.
 
-        Args:
-            self (Director): An instance of Director.
-        """
+    def do_updates(self):
+        """Method/Function:
+        Analyzes if current_car_position is greater than or lesser than self.card
+        and updates current_card.value score based on the users choice of higher or lower and will display 
+        a message and display the current score..
+
+         Args:
+        self (game_master): An instance of game_master"""
+        # analyzes score using player input
+
         print(f"Your score is: {self.total_score}\n")
         if not self.is_playing:
-            return 
+            return
 
         current_card_position = len(self.cards) - 1
 
@@ -76,10 +82,11 @@ class Director:
         self.total_score += new_card.points
 
     def do_outputs(self):
-        """Displays the card and the score.
+        """Method/Function:
+        Will display the new card drawn and your score, else: game is over if user has run out point and chose to end the game.
 
-        Args:
-            self (Director): An instance of Director.
+         Args:
+        self (game_master): An instance of game_master
         """
         if not self.is_playing:
             return
